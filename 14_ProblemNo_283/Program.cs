8ea2f4c165f414f9444d0220c151c677a5004736
KishorekumarPooruli <kishorekumar.pooruli@outlook.com>
2025-06-28 09:36:42 +0100

    update

---
+++ 14_ProblemNo_283/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _14_ProblemNo_283
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.MoveZeroes(new int[] { 0, 1, 0, 3, 12 });
+        }
+    }
+
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int countZeros = nums.Count(y => y == 0);
+            if (countZeros > 0) 
+            {
+                List<int> nonZeroes = nums.ToList();
+                nonZeroes.RemoveAll(y => y == 0);
+
+                for (int i = 0; i < countZeros; i++)
+                {
+                    nonZeroes.Add(0);
+                }
+
+                int[] result = nonZeroes.ToArray();
+                Array.Copy(result, nums, result.Length);
+            }
+        }
+    }
+}
--- 1_ProblemNo1/obj/Debug/net8.0/1_ProblemNo1.AssemblyInfo.cs
+++ 1_ProblemNo1/obj/Debug/net8.0/1_ProblemNo1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa5e14c78622b1654fdb4d9cef556f663b02ddb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d10b5d374a12869ed83a90cef3262673a9c9e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
