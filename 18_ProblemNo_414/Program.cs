c59a922aa1a8b3a32647931cdd4d13ae9f2ffe87
KishorekumarPooruli <kishorekumar.pooruli@outlook.com>
2025-06-29 10:34:47 +0100

    update problem 414

---
+++ 18_ProblemNo_414/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _18_ProblemNo_414
+{
+    /// <summary>
+    /// https://leetcode.com/problems/third-maximum-number/description/?envType=problem-list-v2&envId=array
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.ThirdMax(new int[] { 3,2,1});
+        }
+    }
+
+    public class Solution
+    {
+        public int ThirdMax(int[] nums)
+        {
+            int output = 0;          
+            var distinctInput = nums.Distinct().OrderByDescending(c=>c).ToArray();
+            int[] inputArray = new int[distinctInput.Length];
+            Array.Copy(distinctInput, inputArray, distinctInput.Length);
+
+            
+            int maxLength = inputArray.Length;
+            if(maxLength >= 3)
+            {
+                output = inputArray[3 - 1];
+            }
+            else
+            {
+                output = inputArray.Max();
+            }
+
+            return output;
+        }
+    }
+}
--- 1_ProblemNo1/obj/Debug/net8.0/1_ProblemNo1.AssemblyInfo.cs
+++ 1_ProblemNo1/obj/Debug/net8.0/1_ProblemNo1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4349f7994c2cd47bf5288dd905a808fad745c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5dd80dce54cbbf8e8635f9f9f04887c9c3477c")]
 [assembly: System.Reflection.AssemblyProductAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1_ProblemNo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
