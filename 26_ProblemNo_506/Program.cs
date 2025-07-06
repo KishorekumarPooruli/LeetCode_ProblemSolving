6c3c53936fb1dcfe9897744cfbede4709c83e6f5
KishorekumarPooruli <kishorekumar.pooruli@outlook.com>
2025-07-06 09:30:45 +0100

    updtae

---
+++ 26_ProblemNo_506/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _26_ProblemNo_506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.FindRelativeRanks(new int[] { 10, 3, 8, 9, 4 });
+            //// O(1) < O(log n) < O(n) < O(n log n) < O(n²) < O(n³) < O(2ⁿ) < O(n!)
+            //// This below program is O(n log n) or O(n²) TIME COMPLEXITY 
+        }
+    }
+
+    public class Solution()
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            int[] input = new int[score.Length];
+            Array.Copy(score, input, score.Length);
+            Array.Sort(input);
+            Array.Reverse(input);
+
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+            int counter = 1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (counter <= 3)
+                {
+                    if (counter == 1)
+                    {
+                        keyValuePairs.Add(input[i], "Gold Medal");
+                    }
+                    else if (counter == 2)
+                    {
+                        keyValuePairs.Add(input[i], "Silver Medal");
+                    }
+                    else
+                    {
+                        keyValuePairs.Add(input[i], "Bronze Medal");
+                    }
+                }
+                else
+                {
+                    keyValuePairs.Add(input[i], counter.ToString());
+                }
+
+                counter++;
+            }
+
+            List<string> output = new List<string>();
+
+            for (int j = 0; j < score.Length; j++)
+            {
+                //// since FIRST() is used again it iterates and find the element instread use INDEX TO OPTIMIZE THE CODE 
+                ////var keyValue = keyValuePairs.First(y => y.Key == score[j]);
+                ////output.Add(keyValue.Value);
+                var instance = keyValuePairs[score[j]];
+                output.Add(instance);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
---
+++ 27_ProblemNo_561/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _27_ProblemNo_561
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.ArrayPairSum(new int[] { -9046, -1719, 11, 41, 279, 1118, 2047, 3855, 4925, 7380, 8446, 8477 });
+        }
+    }
+
+    public class Solution
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            ////nums = nums.Distinct().ToArray();
+            Array.Sort(nums);
+            List<ValueTuple<int, int>> generatedCombinations = new List<ValueTuple<int, int>>();
+            int pairs = nums.Length / 2;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var afterCurrentValues = nums.Skip(i +1 ).ToArray();
+
+                foreach(var instance in afterCurrentValues)
+                {
+                    generatedCombinations.Add(new ValueTuple<int, int>(nums[i], instance));
+                }
+            }
+
+            //////foreach(var i in nums)
+            //////{
+            //////    int currentIndex = Array.FindIndex(nums, i, );
+
+            //////}
+
+            int column = generatedCombinations.Count / pairs;
+            ValueTuple<int, int>[,] inputDataObject = new ValueTuple<int, int>[pairs, column];
+            int temp = 0;
+            for (int i = 0; i < inputDataObject.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputDataObject.GetLength(1); j++)
+                {
+                    inputDataObject[i, j] = generatedCombinations[temp];
+                    temp++;
+                }
+            }
+
+            string outputx = inputDataObject.ToString();
+            List<int> output = new List<int>();
+            for (int i = 0; i < column; i++)
+            {
+                List<int> rowSums = new List<int>();
+                for (int j = 0; j < pairs; j++)
+                {
+                    rowSums.Add(Math.Min(inputDataObject[j, i].Item1, inputDataObject[j, i].Item2));
+                }
+
+                int sum = 0;
+                foreach(var rowSum in rowSums)
+                {
+                    sum += rowSum;
+                }
+
+                output.Add(sum);
+            }
+
+
+            return output.Max();
+        }
+    }
+}
